2303d15ddb669e891a238999dc3907d89c2c0721
REGENCY-14 <120068410+REGENCY-14@users.noreply.github.com>
2025-02-09 18:25:43 +0000

    background mobile version

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import SideNavBar from './components/SideNavBar'
-import "./App.css"
+import React from "react";
+import "./App.css";
+import WatchNow from "./components/WatchNow";
 
 export default function App() {
   return (
-    <div>
-      <SideNavBar/>
-    </div>
-  )
+    <>
+      <div className=" relative bg-[#0A071E] h-screen">
+        <div className="absolute right-0 top-0">
+          <img src="/images/Ellipse 1002.png" alt="" />
+        </div>
+        <div className="opacity-[50%] bg-gradient w-screen">
+          <img src="/images/ragnar.jpg" alt="" />
+          <div className="absolute inset-0 bg-gradient-to-b from-black/[0.3] via-[#0A071E] to-black"></div>
+        </div>
+        <div className="">
+          <WatchNow />
+        </div>
+      </div>
+    </>
+  );
 }
---
+++ src/components/WatchNow.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+export default function WatchNow() {
+  return (
+    <>
+      <div>
+        <h1 className="text-white">
+          TV SHOW AND MORE <br /> <span>FIND MOVIES</span>
+        </h1>
+      </div>
+    </>
+  );
+}
